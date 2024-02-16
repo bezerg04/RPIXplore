45ce162d68d4b4e134b8a8a489296ae6678fe045
vicc233 <caiv@rpi.edu>
2024-02-16 17:05:27 -0500

    setting up flowbite react tailwindcss next.js

---
+++ test2/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from 'flowbite-react';
+import { Alert } from 'flowbite-react'
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <Alert>HELLO</Alert>
+    </div>
+
+  );
+}
