f8da0c62c46c289c22b1d508cbb5dd027465a7c2
vicc233 <caiv@rpi.edu>
2024-02-23 17:40:56 -0500

    created the navbar component

--- test2/app/page.js
+++ test2/app/page.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Button } from 'flowbite-react';
 import { Alert } from 'flowbite-react';
-import { Nnn } from './components/navbar.js';
-import { Navbar } from 'flowbite-react';
+import NavBar from '@/component/navbar/page';
 
 export default function Home() {
   return (
-    
-      
-      <Nnn></Nnn>
-      
-    
-
+    <div>
+      <NavBar></NavBar>
+    </div>
+   
   );
 }
---
+++ test2/component/navbar/page.js
@@ -0,0 +1,29 @@
+'use client';
+import logo from '../logo.png'
+import Link from 'next/link';
+import { Navbar } from 'flowbite-react';
+
+
+export default function NavBar() {
+
+  return (
+    <Navbar fluid rounded>
+    <Navbar.Brand as={Link} href="https://flowbite-react.com">
+      <img src={logo.src} className="mr-3 h-9 sm:h-9 " alt="Flowbite React Logo" />
+      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">RPIXplore</span>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse>
+      <Navbar.Link href="#" active>
+        Home
+      </Navbar.Link>
+      <Navbar.Link as={Link} href="#">
+        Forum
+      </Navbar.Link>
+      <Navbar.Link href="#">Resources</Navbar.Link>
+      <Navbar.Link href="#">Dorms</Navbar.Link>
+      <Navbar.Link href="#">Login</Navbar.Link>
+    </Navbar.Collapse>
+  </Navbar>
+  );
+}
