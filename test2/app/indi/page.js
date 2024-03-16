9e08cc53b3a63dbb92177540fd1b1fb9c540fee8
bezerg04 <bezerg@rpi.edu>
2024-03-16 00:47:47 -0400

    small fixes, indi work

--- test2/app/indi/page.js
+++ test2/app/indi/page.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Button } from 'flowbite-react';
-import { Alert } from 'flowbite-react';
 import NavBar from '@/component/navbar/page';
 import Jumbotron from '@/component/jumbotron/page';
-import Welcome from '@/component/welcome/page';
-import Tritiles from '@/component/tritiles/page';
-import MapComponent from '@/component/map/page';
 import Sector from '@/component/sector/page';
 
-export default function Home({ child = 'quad' }) {
+export default function IndiPage({ child = 'Quad' }) {
   const jumbotronImage = require(`@/component/jumbotron/${child}jumbo.png`).default;
 
   return (
--- test2/component/sector/page.js
+++ test2/component/sector/page.js
@@ -1,29 +1,68 @@
-'use client';
+'use client'
 import { useState } from 'react';
 import Link from 'next/link';
-import data from '../../../resources/json.json'
+import { Button } from 'flowbite-react';
+import { Carousel } from 'flowbite-react';
+
+
+import data from '../../../resources/json.json';
 
 export default function Sector({ sectionName = 'Quad' }) {
-    // Dynamically access the section of the data based on the prop
-    const sectionData = data[sectionName];
-    
-    // Ensure that sectionData is not undefined
-    if (!sectionData) {
-      return <div>Section data not found.</div>;
-    }
+  const sectionData = data[sectionName];
   
-    return (
-      <div>
-        {/* Displaying information from the dynamically selected section */}
-        <h1>{sectionName} Residence Hall</h1>
-        <p><strong>Description:</strong> {sectionData.description}</p>
-        <p><strong>Address:</strong> {sectionData.address}</p>
-        <p><strong>Location:</strong> {sectionData.location}</p>
-        <p><strong>Accessible:</strong> {sectionData.accessible ? 'Yes' : 'No'}</p>
-        <p><strong>AC Available:</strong> {sectionData.ac ? 'Yes' : 'No'}</p>
-        <p><strong>Dining:</strong> {sectionData.dining}</p>
-        <p><strong>Laundry Info:</strong> Located at {sectionData.laundryInfo.location} with {sectionData.laundryInfo.nWashers} washers and {sectionData.laundryInfo.nDryers} dryers.</p>
-        {/* Continue to add more information as needed */}
-      </div>
-    );
+  if (!sectionData) {
+    return <div>Section data not found.</div>;
   }
+
+  // Destructuring for easier access
+  const { pricing, stacks, description, address, location, accessible, ac, dining, laundryInfo } = sectionData;
+  
+  return (
+    <div>
+
+      {/* Container for buttons with full width, centered content, and padding */}
+      <div className=' w-10/12 flex justify-center items-center p-4'>
+        {stacks && stacks.length > 1 && (
+          <div className='flex gap-1 flex-wrap justify-center'>
+            {stacks.map((stack, index) => (
+                <Button key={index} color="gray">{stack.title}</Button>
+            ))}
+          </div>
+        )}
+      </div>
+      
+      <div className="flex flex-column">
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <Carousel>
+        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+      </Carousel>
+    </div>
+        <div>
+            <p><strong>Description:</strong> {description}</p>
+            <p><strong>Address:</strong> {address}</p>
+            <p><strong>Location:</strong> {location}</p>
+            <p><strong>Accessible:</strong> {accessible ? 'Yes' : 'No'}</p>
+            <p><strong>AC Available:</strong> {ac ? 'Yes' : 'No'}</p>
+            <p><strong>Nearest Dining Hall:</strong> {dining}</p>
+            <p><strong>Laundry Info:</strong> Located at {laundryInfo.location} with {laundryInfo.nWashers} washers and {laundryInfo.nDryers} dryers.</p>
+            <p><strong>Pricing:</strong></p>
+              <ul>
+                <li> &emsp; Single: {pricing.single}</li>
+                <li> &emsp; Double: {pricing.double}</li>
+                <li> &emsp; Triple: {pricing.triple}</li>
+              </ul>
+            
+        
+        </div>
+
+      </div>
+
+        
+      
+    </div>
+  );
+}
