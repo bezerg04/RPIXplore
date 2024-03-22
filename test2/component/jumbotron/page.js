9028634f829c4e870fdb88c17f61efcab8aaf1aa
vicc233 <caiv@rpi.edu>
2024-03-22 16:43:35 -0400

    styling for dorm cards

--- test2/component/jumbotron/page.js
+++ test2/component/jumbotron/page.js
@@ -3,12 +3,8 @@ import Link from 'next/link';
 
 const Jumbotron = ({ imageSrc }) => {
   return (
-    <div className="relative w-full bg-center bg-no-repeat" style={{ paddingTop: 'calc(100% / (aspect-ratio))' }}>
-      <img src={imageSrc} alt="" className="top-0 left-0 w-full h-auto " />
-      <div className='text-block absolute bottom-20 right-20 bg-black text-white p-5'>
-        <h4>Nature</h4>
-        <p>What a beautiful sunrise</p>
-      </div>
+    <div className="relative w-full" style={{ paddingTop: '33.333%' }}> 
+      <img src={imageSrc} alt="barton hall" className="absolute top-0 left-0 w-full h-full object-cover" />
     </div>
     
   );
