31daf77d1ff606c9c061d281ab03816038022f8c
bezerg04 <bezerg@rpi.edu>
2024-07-19 14:11:50 -0400

    it worked but the website looks wrong

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['js', 'jsx'],
-    experimental: {
-      appDir: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  pageExtensions: ['js', 'jsx'],
+};
+
+export default nextConfig;
