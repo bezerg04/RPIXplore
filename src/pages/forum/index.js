286f88f6049713ea0d624b93f40d759768eb5933
bezerg04 <bezerg@rpi.edu>
2024-11-20 10:56:15 -0500

    forum imgs and navbar

--- src/pages/forum/index.js
+++ src/pages/forum/index.js
@@ -5,6 +5,14 @@ import { useAuth } from "../../context/AuthContext";
 import Post from "../../component/Post/Post";
 import CreatePost from "../../component/CreatePost/CreatePost";
 import AdminControls from "../../component/AdminControls/AdminControls";
+import NavBar from "@/src/component/navbar/page";
+import Jumbotron2 from "@/src/component/JumbotronWithText/page";
+import jumbo from '@/src/component/JumbotronWithText/jumbo.png';
+import one from '@/src/component/JumbotronWithText/one.jpg';
+import two from '@/src/component/JumbotronWithText/two.jpg';
+import three from '@/src/component/JumbotronWithText/three.jpg';
+
+
 
 export default function Forum() {
   const { user } = useAuth();
@@ -28,6 +36,16 @@ export default function Forum() {
   );
 
   return (
+    <div>
+      <NavBar activeLink="Forum" />
+      <Jumbotron2
+        titleText="Welcome to RPXplore!"
+        subtitleText="Explore the campus and learn more about what it has to offer."
+        lowerText={`Forum,\nwith RPIXplore`}
+        backgroundImage={two.src}
+        showBottom={true}
+        showTop={false}
+      />
     <div className="container mx-auto px-4 py-6">
       <h1 className="text-3xl font-bold text-center mb-6">Forum</h1>
       {user && <CreatePost user={user} isAdmin={isAdmin} onPostCreated={fetchPosts} />
@@ -49,6 +67,7 @@ export default function Forum() {
           ))
         )}
       </div>
+    </div>
     </div>
   );
 }
